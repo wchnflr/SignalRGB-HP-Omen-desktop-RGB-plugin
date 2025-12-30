871daa5bd858499f99095554aadaa547181ee2d7
Wayne <118108543+wchnflr@users.noreply.github.com>
2025-12-30 17:22:46 -0500

    Enable unknown LED outputs
    Assign unique colors to unknown LEDs at addresses 20 through 46.

--- HP_Omen_Desktop_RGB.js
+++ HP_Omen_Desktop_RGB.js
@@ -108,15 +108,52 @@ function sendColors(shutdown = false)
 //    packet[17] = col[0] // data 17 - CPU Cooler Red Value in hex, Leave at 0x00 for other modes
 //    packet[18] = col[1] // data 18 - CPU Cooler Green Value in hex, Leave at 0x00 for other modes
 //    packet[19] = col[2] // data 19 - CPU Cooler Blue Value in hex, Leave at 0x00 for other modes
-    packet[48] = 0x64 // data 48 - Changes Brightness 25% (0x19), 50% (0x32), 75% (0x4b), 100% (0x64)
-    packet[49] = 0x02 // data 49 - 0x0a or 0x04 for system vitals, constantly changing stuff - leave at 0x0a; 0x02 for static color
+    packet[20] = 255  // data 20 - Unknown Red Value in hex, Leave at 0x00 for other modes
+    packet[21] = 0    // data 21 - Unknown Green Value in hex, Leave at 0x00 for other modes
+    packet[22] = 0    // data 22 - Unknown Blue Value in hex, Leave at 0x00 for other modes
+
+	packet[23] = 0    // data 23 - Unknown Red Value in hex, Leave at 0x00 for other modes
+    packet[24] = 255  // data 24 - Unknown Green Value in hex, Leave at 0x00 for other modes
+    packet[25] = 0    // data 25 - Unknown Blue Value in hex, Leave at 0x00 for other modes
+
+	packet[26] = 0    // data 26 - Unknown Red Value in hex, Leave at 0x00 for other modes
+    packet[27] = 0    // data 27 - Unknown Green Value in hex, Leave at 0x00 for other modes
+    packet[28] = 255  // data 28 - Unknown Blue Value in hex, Leave at 0x00 for other modes
+
+	packet[29] = 255  // data 29 - Unknown Red Value in hex, Leave at 0x00 for other modes
+    packet[30] = 255  // data 30 - Unknown Green Value in hex, Leave at 0x00 for other modes
+    packet[31] = 0    // data 31 - Unknown Blue Value in hex, Leave at 0x00 for other modes
+
+	packet[32] = 0    // data 32 - Unknown Red Value in hex, Leave at 0x00 for other modes
+    packet[33] = 255  // data 33 - Unknown Green Value in hex, Leave at 0x00 for other modes
+    packet[34] = 255  // data 34 - Unknown Blue Value in hex, Leave at 0x00 for other modes
+
+	packet[35] = 255  // data 35 - Unknown Red Value in hex, Leave at 0x00 for other modes
+    packet[36] = 0    // data 36 - Unknown Green Value in hex, Leave at 0x00 for other modes
+    packet[37] = 255  // data 37 - Unknown Blue Value in hex, Leave at 0x00 for other modes
+
+	packet[38] = 127  // data 38 - Unknown Red Value in hex, Leave at 0x00 for other modes
+    packet[39] = 0    // data 39 - Unknown Green Value in hex, Leave at 0x00 for other modes
+    packet[40] = 0    // data 40 - Unknown Blue Value in hex, Leave at 0x00 for other modes
+
+	packet[41] = 0    // data 41 - Unknown Red Value in hex, Leave at 0x00 for other modes
+    packet[42] = 127  // data 42 - Unknown Green Value in hex, Leave at 0x00 for other modes
+    packet[43] = 0    // data 43 - Unknown Blue Value in hex, Leave at 0x00 for other modes
+
+	packet[44] = 0    // data 44 - Unknown Red Value in hex, Leave at 0x00 for other modes
+    packet[45] = 0    // data 45 - Unknown Green Value in hex, Leave at 0x00 for other modes
+    packet[46] = 127  // data 46 - Unknown Blue Value in hex, Leave at 0x00 for other modes
+
+	packet[48] = 0x64 // data 38 - Changes Brightness 25% (0x19), 50% (0x32), 75% (0x4b), 100% (0x64)
+	packet[49] = 0x02 // data 49 - 0x0a or 0x04 for system vitals, constantly changing stuff - leave at 0x0a; 0x02 for static color
     packet[50] = 0x00 // data 50 - 0x00 from Wireshark
-    packet[51] = 0x00 // data 51 - 0x00 from Wireshark
+	packet[51] = 0x00 // data 51 - 0x00 from Wireshark
     packet[52] = 0x00 // data 52 - 0x00 from Wireshark
     packet[53] = 0x00 // data 53 - 0x00 from Wireshark
-    packet[54] = 0x00 // data 54 - LED Module ID - Front is 0x01, Inside LED Bar is 0x02, CPU Cooler is 0x04, all lights is 0x00
+	packet[54] = 0x00 // data 54 - LED Module ID - Front is 0x01, Inside LED Bar is 0x02, CPU Cooler is 0x04, all lights is 0x00
     packet[55] = 0x01 // data 55 - 0x01 for the lighting for the computer is on. 0x02 when the computer is suspend.
     packet[56] = 0x00 // data 56 - Changes the Theme - Galaxy (0x01), Volcano (0x02), Jungle (0x03), Ocean (0x04) etc.; 0x00 for static
     packet[57] = 0x00 // data 57 - Changes the speed Slow (0x01), Medium (0x02), Fast (0x03); 0x00 for static
     device.write(packet, 58);
 }
+
